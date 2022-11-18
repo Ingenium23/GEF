70c137115b2b8bc1cc977c4dc70cddca872dac0f
Ingenium23 <100562792+Ingenium23@users.noreply.github.com>
2022-11-18 23:23:48 +0300

    Update settings.py

--- greeningEarth/settings.py
+++ greeningEarth/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-%#l_12f7vp)j3n1q%4)o_dq46ykb$2g8i0mly8!jtk)aetc9=c'
+
 SECRET_KEY = os.environ.get('G_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'True'
